dbe3bb5749201655b2997fa6e286258028c4cde5
Diogo Soares <dioggosoares35@gmail.com>
2023-02-07 14:05:16 -0300

    feat: add new icons

--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,4 @@
 export { ArchiveBox } from './icons/archives-folders/ArchiveBox'
 export { Bookmark } from './icons/bookmark/Bookmark'
 export { BookmarkAdd } from './icons/bookmark/BookmarkAdd'
+export { BookmarkBlocked } from './icons/bookmark/BookmarkBlocked'
