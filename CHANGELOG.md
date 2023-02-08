bb4efa308d5962b570736ff87d52fe6430196e0b
Diogo Soares <dioggosoares35@gmail.com>
2023-02-08 18:21:17 -0300

    chore(lib): delete commented code

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,11 @@
 # uicons-react
 
+## 0.1.8
+
+### Patch Changes
+
+- delete commented code
+
 ## 0.1.7
 
 ### Patch Changes
--- src/icons/warning-check/Add.tsx
+++ src/icons/warning-check/Add.tsx
@@ -147,37 +147,6 @@ export const Add = forwardRef<SVGSVGElement, Icon>((props, ref) => {
         strokeLinejoin="round"
         d="M12 18V6"
       />
-
-      {/* <path
-        fill={customColors?.fill === undefined ? 'none' : customColors?.fill}
-        stroke={
-          customColors?.outter === undefined ? '#000000' : customColors?.outter
-        }
-        strokeWidth={1.5}
-        strokeLinecap="round"
-        strokeLinejoin="round"
-        d="M16.8203 2H7.18031C5.05031 2 3.32031 3.74 3.32031 5.86V19.95C3.32031 21.75 4.61031 22.51 6.19031 21.64L11.0703 18.93C11.5903 18.64 12.4303 18.64 12.9403 18.93L17.8203 21.64C19.4003 22.52 20.6903 21.76 20.6903 19.95V5.86C20.6803 3.74 18.9503 2 16.8203 2Z"
-      />
-      <path
-        stroke={
-          customColors?.inner === undefined ? '#000000' : customColors?.inner
-        }
-        strokeWidth={1.5}
-        strokeMiterlimit="10"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-        d="M14.5 10.65H9.5"
-      />
-      <path
-        stroke={
-          customColors?.inner === undefined ? '#000000' : customColors?.inner
-        }
-        strokeWidth={1.5}
-        strokeMiterlimit="10"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-        d="M12 8.20999V13.21"
-      /> */}
     </>
   ))
 
